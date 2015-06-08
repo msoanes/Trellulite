62c5f6e114091ce10a50fd5767c15b79a5f8f02d
Marcel Soanes <m.l.soanes@gmail.com>
2015-06-08 11:57:45 -0700

    Update board show view and template to render lists

--- app/assets/javascripts/views/boards/boards_show.js
+++ app/assets/javascripts/views/boards/boards_show.js
@@ -1,14 +1,30 @@
-TrelloClone.Views.BoardShow = Backbone.View.extend({
+TrelloClone.Views.BoardShow = Backbone.CompositeView.extend({
   template: JST['boards/show'],
 
   initialize: function () {
+    this.addLists();
     this.listenTo(this.model, 'sync', this.render);
+    this.listenTo(this.model.lists(), 'add', this.addLists);
   },
 
   render: function () {
-    console.log('rendering');
     var renderedContent = this.template({ board: this.model });
     this.$el.html(renderedContent);
+    this.attachSubviews();
     return this;
+  },
+
+  addLists: function () {
+    var view = this;
+    this.model.lists().each(function (model) {
+      view.addList(model);
+    });
+  },
+
+  addList: function (model) {
+    var listView = new TrelloClone.Views.ListShow({
+      model: model
+    });
+    this.addSubview('.lists', listView);
   }
 });
