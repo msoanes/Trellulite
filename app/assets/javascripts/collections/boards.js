5c910280097b8e75915ff7141e9250e17eb59fd2
Marcel Soanes <m.l.soanes@gmail.com>
2015-06-08 10:57:32 -0700

    Add getOrFetch to Boards collection

--- app/assets/javascripts/collections/boards.js
+++ app/assets/javascripts/collections/boards.js
@@ -1,5 +1,19 @@
 TrelloClone.Collections.Boards = Backbone.Collection.extend({
   url: '/api/boards',
 
-  model: TrelloClone.Models.Board
+  model: TrelloClone.Models.Board,
+
+  getOrFetch: function (id) {
+    var collection = this;
+    var model = collection.get(id);
+    if (!model) {
+      model = new this.model({id: id});
+      model.fetch({
+        success: function () {
+          collection.add(model);
+        }
+      });
+    }
+    return model;
+  }
 });
