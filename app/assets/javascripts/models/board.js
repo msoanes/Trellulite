d0ca21e234b0fbc01a18c033cad9510264d17329
Marcel Soanes <m.l.soanes@gmail.com>
2015-06-08 11:40:37 -0700

    Add parse method to Board model

--- app/assets/javascripts/models/board.js
+++ app/assets/javascripts/models/board.js
@@ -1,3 +1,22 @@
 TrelloClone.Models.Board = Backbone.Model.extend({
-  urlRoot: '/api/boards'
+  urlRoot: '/api/boards',
+
+  parse: function (payload) {
+    var board = this;
+    if (payload.lists) {
+      _.each(payload.lists, function (listData) {
+        var list = new TrelloClone.Models.List(listData, { parse: true });
+        board.lists().add(list);
+      });
+      delete payload.lists;
+    }
+    return payload;
+  },
+
+  lists: function () {
+    if (!this._lists) {
+      this._lists = new TrelloClone.Collections.Lists();
+    }
+    return this._lists;
+  }
 });
