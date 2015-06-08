ec9f39a9afd9b3caa89a9949058c8a2c116a1d36
Marcel Soanes <m.l.soanes@gmail.com>
2015-06-08 11:24:44 -0700

    Create form view and template

---
+++ app/assets/javascripts/views/boards/board_form.js
@@ -0,0 +1,30 @@
+TrelloClone.Views.BoardForm = Backbone.View.extend({
+  template: JST['boards/form'],
+
+  tagName: 'form',
+
+  events: {
+    'submit': 'submit'
+  },
+
+  render: function () {
+    console.log('rendering');
+    var renderedContent = this.template();
+    this.$el.html(renderedContent);
+    return this;
+  },
+
+  submit: function (event) {
+    event.preventDefault();
+    var data = this.$el.serializeJSON();
+    this.model.save(data, {
+      success: this.successfulSubmit.bind(this)
+    });
+  },
+
+  successfulSubmit: function () {
+    this.$el[0].reset();
+    this.collection.add(this.model);
+    this.model = new TrelloClone.Models.Board();
+  }
+});
