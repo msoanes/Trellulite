4a87838a59aa52a529edee962934f26fad50cb15
Marcel Soanes <m.l.soanes@gmail.com>
2015-06-08 10:58:25 -0700

    Add boardShow method to router

--- app/assets/javascripts/routers/router.js
+++ app/assets/javascripts/routers/router.js
@@ -6,13 +6,22 @@ TrelloClone.Routers.Router = Backbone.Router.extend({
 
   routes: {
     '': 'boardsIndex',
-    'boards/:id': 'boardsShow'
+    'boards/:id': 'boardShow'
   },
 
   boardsIndex: function () {
     this.collection.fetch();
     var view = new TrelloClone.Views.BoardsIndex({
       collection: this.collection
+    });
+
+    this._swapView(view);
+  },
+
+  boardShow: function (id) {
+    var board = this.collection.getOrFetch(id);
+    var view = new TrelloClone.Views.BoardShow({
+      model: board
     });
 
     this._swapView(view);
