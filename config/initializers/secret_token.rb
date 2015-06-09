a7393ba6f3312e32347e14c615ff5b6dad4164b1
Marcel Soanes <m.l.soanes@gmail.com>
2015-06-09 00:50:35 -0700

    Fix a bad copy-paste error

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -1,2 +1,2 @@
-MyApp::Application.config.secret_key_base = ENV['SECRET_KEY_BASE'] || 'insecure_token' 
+TrelloClone::Application.config.secret_key_base = ENV['SECRET_KEY_BASE'] || 'insecure_token' 
 
